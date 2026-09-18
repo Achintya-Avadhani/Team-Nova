9a40986d09289a9252064021d6abb7c02182ba87
Ketaki <ketakichalke4@gmail.com>
2026-09-18 10:08:45 +0530

    signup function added

--- Backend/src/main/java/com/hdfc/secureauth/config/SecurityConfig.java
+++ Backend/src/main/java/com/hdfc/secureauth/config/SecurityConfig.java
@@ -13,7 +13,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/login", "/signup", "/auth", "/logout").permitAll()
                         .anyRequest().authenticated()
                 );
         return http.build();
