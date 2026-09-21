99822f7e582b8e3380ac57c8af57b2375ae0d692
Shreya <shreya180304@gmail.com>
2026-09-21 11:24:56 +0530

    Implement HttpOnly cookie authentication

--- Backend/src/main/java/com/hdfc/secureauth/config/CorsConfig.java
+++ Backend/src/main/java/com/hdfc/secureauth/config/CorsConfig.java
@@ -16,7 +16,8 @@ public class CorsConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173",
+                List.of("http://localhost:3000",
+                        "http://localhost:5173",
                         "http://192.168.1.109:5173")
 
         );
